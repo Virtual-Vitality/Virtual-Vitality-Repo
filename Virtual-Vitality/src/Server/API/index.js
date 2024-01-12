adacd8438d459e160868a258b6a7f3f1bf555627
Jaylen <jmowens90@gmail.com>
2024-01-12 13:09:54 -0500

    added the base end routes to the server/main.js and server/API/index.js

---
+++ Virtual-Vitality/src/Server/API/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+// /API/coaches
+router.use("/coaches", require("./coaches"));
+
+// /API/exercises
+router.use("/exercises", require("./exercises"));
+
+// /API/users
+router.use("/users", require("./users"));
+
+// /API/workouts
+router.use("/workouts", require("./workouts"));
+
+router.use("/", (req, res) => {
+    res.send(`Welcome to the API`);
+})
+
+module.exports = router;
