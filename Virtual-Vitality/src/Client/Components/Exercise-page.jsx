2e1f4dae3c50c4e181b023fe5a2d2ad4e80e01af
Jaylen <jmowens90@gmail.com>
2024-01-24 11:49:05 -0500

    was able to change the styling of the exercise cards so that all of the videos lined up symmetrically

--- Virtual-Vitality/src/Client/Components/Exercise-page.jsx
+++ Virtual-Vitality/src/Client/Components/Exercise-page.jsx
@@ -23,14 +23,17 @@ const Exercises= () =>{
         <div >
         <div className="grid grid-cols-3 gap-4">
           {exercises.map((exercise) => (
-            <Card style={{ width: '18rem' }} key={exercise.id}>
+            <Card className=' relative' style={{ width: '18rem'}} key={exercise.id}>
               <Card.Img width="100%" height="150" variant="top" src={exercise.imgUrl} alt='Exercise Photo' />
               <Card.Body >
                 <Card.Title>{exercise.name}</Card.Title>
                 <Card.Text> Muscle Group: {exercise.muscleGroup}</Card.Text>
                 <Card.Text> Description: {exercise.description}</Card.Text>
-                <iframe width="250" height="150" src={exercise.videoLink} variant="bottom" title="Exercise Video | Push Ups Video For Editing | Gym Video | Stock Video | Free Footage | Free Video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                <br/>
               </Card.Body>
+                <div className=' m-auto mb-3'>
+                <iframe width="250" height="150" src={exercise.videoLink} variant="bottom" title="Exercise Video | Push Ups Video For Editing | Gym Video | Stock Video | Free Footage | Free Video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                </div>
             </Card>
           ))}
         </div>
