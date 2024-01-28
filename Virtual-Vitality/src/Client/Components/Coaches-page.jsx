f46f9496e4e5a021a31faa13cfb5438ba5dc7e70
algorithm_artisit <algorithm_artist@DESKTOP-SV23GD5>
2024-01-28 15:13:46 -0800

    set up coaches and workouts as well as tested all routes apt needs work

--- Virtual-Vitality/src/Client/Components/Coaches-page.jsx
+++ Virtual-Vitality/src/Client/Components/Coaches-page.jsx
@@ -1,6 +1,27 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import React,{useState} from 'react';
+import axios from 'axios';
+
 const example_function8= () =>{
+    //create endpoint get call
+    //add handleSubmit
+    //use useState to track inputs and save them to variables to input
+    //into get request
+    const [nameOutput, setnameOutput] =useState(``);
+    const [imgOutput, setimgOutput] =useState(``)
+    
+  const handleSubmit = async(event) =>{
+    event.preventDefault();
+    try{
+      const result = await axios.get("/api/coaches")
+    }catch(error){
+      console.log(error)
+    }
+    console.log(result)
+  } 
+handleSubmit();
+
     return(
     <div
       className="modal show"
