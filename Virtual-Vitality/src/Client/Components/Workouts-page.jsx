6523aba89c0bb266058d894bfe9284676b28ea46
Peng Cui <pengcui@Pengs-MacBook-Pro.local>
2024-01-29 08:14:58 -0800

    fifth test

--- Virtual-Vitality/src/Client/Components/Workouts-page.jsx
+++ Virtual-Vitality/src/Client/Components/Workouts-page.jsx
@@ -1,18 +1,50 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-const Example_function= () =>{
-return(
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import React, { useEffect, useState} from "react";
+
+const workoutPage= () =>{
+    const [workout, setWorkout] = useState([]);
+
+    useEffect(() => {
+        async function getWorkout() {
+            try {
+                const { data: getWorkouts } = await axios.get("/api/workouts");
+                setWorkout(getWorkouts);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        getWorkout();
+    }, []);
+
+       return(
+          <div>
+          <ButtonGroup aria-label="Difficult level">
+          <Button variant="secondary">Beginner</Button>
+          <Button variant="secondary">Intermediate</Button>
+          <Button variant="secondary">Advanced</Button>
+          </ButtonGroup>
+          
+          
+         
+        
+        <div className="grid grid-cols-3 gap-4">
+          {exercises.map((exercise) => (
+
         <Card style={{ width: '18rem' }}>
           <Card.Img variant="top" src="holder.js/100px180" />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>workout.name</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              workoutType
             </Card.Text>
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
+          ))}
         </Card>
+        </div>
+        </div>
 )
 }
-export default Example_function
+export default workoutPage
