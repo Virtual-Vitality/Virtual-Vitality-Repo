25a8d148700460eb75d50e24a674201f181e8067
algorithm_artisit <algorithm_artist@DESKTOP-SV23GD5>
2024-01-29 20:01:33 -0800

    added functionality to scheduler and styles *need handleSubmit for form, onClick for coach prefernece and get request to send all info*

--- Virtual-Vitality/src/Client/Components/calendar.jsx
+++ Virtual-Vitality/src/Client/Components/calendar.jsx
@@ -1,3 +1,5 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import DatePicker from "react-datepicker";
 import { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
@@ -5,12 +7,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {FaCalendarAlt} from "react-icons/fa"
 
 const Scheduler = () => {
+    //useState to track handle submit of coach into post request
     const [selectedDate, setselectedDate] = useState(null)
     return (
-      <>
-      <div className= " flex items-center justify-center  bg-slate-700 rounded-md p-2"> 
-      <label className=" bg-black text-cyan-50">
-        <p>This is the calendar</p>
+      
+      <div className= " flex items-center justify-center  bg-slate-700 rounded-md p-3"> 
+        <form>
+        <h1 className=' bg-black text-cyan-50'>Schedule Your Appointment</h1>
+        <br />
+        <br />
       <DatePicker 
        className="bg-slate-500 rounded-md p-2"
       selected ={selectedDate} 
@@ -19,10 +24,24 @@ const Scheduler = () => {
       minDate={new Date()}
       filterDate={date => date.getDay() != 6 && date.getDay() != 0}
       isClearable
+      showTimeSelect
       />
-      </label>
+      {console.log(selectedDate)}
+      <br />
+      <br />
+      <DropdownButton id="dropdown-basic-button" title="Choose Your Coach">
+      <Dropdown.Item href="#/action-1">Jamari</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">Jaylen</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">Sebastian</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">Peng</Dropdown.Item>
+    </DropdownButton>
+      <br />
+      <br />
+      <button className=' bg-black text-white p-2 rounded-md'>Submit</button>
+      </form>
+      
       </div>
-      </>
+      
       )
 }
  export default Scheduler
