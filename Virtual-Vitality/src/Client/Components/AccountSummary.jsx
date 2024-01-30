7ba256388350ed7d8714ffc31bdbad7c6e035fdf
Jaylen <jmowens90@gmail.com>
2024-01-30 15:37:45 -0500

    linked the current user to the account page, created working backend routes, still working on front end components

--- Virtual-Vitality/src/Client/Components/AccountSummary.jsx
+++ Virtual-Vitality/src/Client/Components/AccountSummary.jsx
@@ -1,32 +1,50 @@
 //create route and attach it to navbar at bottom/add new route to array and widget.
-import { useState } from 'react';
-import { Button } from 'react-bootstrap';
-import ListGroup from 'react-bootstrap/ListGroup';
-import { useNavigate } from 'react-router';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import ListGroup from "react-bootstrap/ListGroup";
+import { useNavigate } from "react-router";
 
-function AccountSummary( ) {
-    /*Create 2 get requests one to /auth/login to pull user's info and display it in lists format within a label,
+function AccountSummary() {
+  /*Create 2 get requests one to /auth/login to pull user's info and display it in lists format within a label,
     also api/appoitments to populate apt info with coach name too */
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [token, setToken] = useState(
-      window.localStorage.getItem("token")
-    )
+  const [user, setUser] = useState("");
+  const [token, setToken] = useState(window.localStorage.getItem("token"));
 
-    const logOutHandler = () => {
-      localStorage.removeItem("token");
-      setToken(null);
-      navigate("/");
-    }
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const { data: result } = await axios.get("/api/users/me", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        console.log(result)
+        setUser(result);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+    getUser();
+  }, [token]);
+
+  const logOutHandler = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+    navigate("/");
+  };
 
   return (
     <ListGroup>
-      <ListGroup.Item>Username Info</ListGroup.Item>
-      <ListGroup.Item>Password Info</ListGroup.Item>
-      <ListGroup.Item>Email Info</ListGroup.Item>
-      <ListGroup.Item>Name info</ListGroup.Item>
-      <ListGroup.Item>Appointment Info</ListGroup.Item>
-      <Button variant="danger" onClick={logOutHandler}>Logout</Button>{' '}
+      <ListGroup.Item>Name: {user.name}</ListGroup.Item>
+      <ListGroup.Item>Username: {user.userName}</ListGroup.Item>
+      <ListGroup.Item>Email: {user.email}</ListGroup.Item>
+      <ListGroup.Item>Appointments: {user.appointments}</ListGroup.Item>
+      <Button variant="danger" onClick={logOutHandler}>
+        Logout
+      </Button>{" "}
     </ListGroup>
   );
 }
