550b97d7840873c6471f28e5b75ff00b86f6dcdc
Jaylen <jmowens90@gmail.com>
2024-01-30 12:11:31 -0500

    created account page component and have a functioning logout button

--- Virtual-Vitality/src/Client/Components/AccountSummary.jsx
+++ Virtual-Vitality/src/Client/Components/AccountSummary.jsx
@@ -1,9 +1,23 @@
 //create route and attach it to navbar at bottom/add new route to array and widget.
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { useNavigate } from 'react-router';
 
-function summaryFunc() {
+function AccountSummary( ) {
     /*Create 2 get requests one to /auth/login to pull user's info and display it in lists format within a label,
     also api/appoitments to populate apt info with coach name too */
+    const navigate = useNavigate();
+
+    const [token, setToken] = useState(
+      window.localStorage.getItem("token")
+    )
+
+    const logOutHandler = () => {
+      localStorage.removeItem("token");
+      setToken(null);
+      navigate("/");
+    }
 
   return (
     <ListGroup>
@@ -12,8 +26,9 @@ function summaryFunc() {
       <ListGroup.Item>Email Info</ListGroup.Item>
       <ListGroup.Item>Name info</ListGroup.Item>
       <ListGroup.Item>Appointment Info</ListGroup.Item>
+      <Button variant="danger" onClick={logOutHandler}>Logout</Button>{' '}
     </ListGroup>
   );
 }
 
-export default summaryFunc;
+export default AccountSummary;
